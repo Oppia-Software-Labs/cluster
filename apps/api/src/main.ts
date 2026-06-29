98aa42fd4cd089ad8b16eb0853b65037547bca92
aguilar1x <aaguilar1x@gmail.com>
2026-06-29 01:06:16 -0600

    feat: register cookie-parser and AuthModule

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,14 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import type { Env } from './config/env.schema';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
