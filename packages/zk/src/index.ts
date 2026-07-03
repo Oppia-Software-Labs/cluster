926c915dc870c056403c9d38a46a9bddecc1d237
aguilar1x <aaguilar1x@gmail.com>
2026-07-03 01:28:54 -0600

    feat: add witness builders and xdr payload codec with vectors

--- packages/zk/src/index.ts
+++ packages/zk/src/index.ts
@@ -1,3 +1,5 @@
 export type { ProofEnvelope, Opening } from "./types.js";
 export * from "./crypto/index.js";
 export * from "./account/index.js";
+export * from "./witness/index.js";
+export * from "./chain/index.js";
